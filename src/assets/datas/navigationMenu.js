ddd654938c82e6e41e677c02b76e44853d008e09
dduckddak-kim <kkgg7527@gmail.com>
2022-07-07 18:32:32 +0900

    Add. Navigation 메뉴 데이터 파일 추가

---
+++ src/assets/datas/navigationMenu.js
@@ -0,0 +1,16 @@
+const navigationMenu = [
+  {
+    id: 1,
+    name: "home",
+    icon: "",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "list",
+    icon: "",
+    url: "/Product",
+  }
+];
+
+export default navigationMenu;
