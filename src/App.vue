6ac2a7719f44daad83a7c31ffc522536d0cbee87
dduckddak-kim <kkgg7527@gmail.com>
2022-07-07 15:02:49 +0900

    Add. Common Color.scss

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "style/color.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
