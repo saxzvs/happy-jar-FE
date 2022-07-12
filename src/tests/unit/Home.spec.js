361cf4865e5b8d026a4dd973b75545c8f5bc5a1f
dduckddak-kim <kkgg7527@gmail.com>
2022-07-12 13:34:35 +0900

    Test. 'edit' 버튼 관련 TC 추가
    - 버튼 생성 여부
    - 버튼 클릭 시 모달 생성 확인.

--- src/tests/unit/Home.spec.js
+++ src/tests/unit/Home.spec.js
@@ -1,18 +1,26 @@
-import { mount } from '@vue/test-utils';
+import { mount, flushPromises } from '@vue/test-utils';
 
-import Home from "@/views/Home.vue";
-// import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
 
-describe("Home.vue", () => {
+describe('Home.vue', () => {
+  test('"Home" 컴포넌트 렌더링 확인.', () => {
+    const wrapper = mount(Home, {});
 
-	test("1은 1이야.", () => {
-		expect(1).toBe(1);
-	});
+    expect(wrapper.find('[data-test="title"]').exists()).toBe(true);
+  });
 
-	test("renders Home component", () => {
-		const wrapper = mount(Home, {});
+  test('"edit" 버튼 생성 여부.', () => {
+    const wrapper = mount(Home);
 
-		expect(wrapper.find('[data-test="title"]').exists()).toBe(true);
-	});
-})
+    expect(wrapper.find('[data-test="mood-edit-button"]'));
+  });
 
+  test('"edit"버튼 클릭 시, 모달 생성 확인.', async () => {
+    const wrapper = mount(Home);
+
+    await wrapper.find('[data-test="mood-edit-button"]').trigger('click');
+    await flushPromises();
+
+    expect(wrapper.exists('[data-test="modal"]')).toBe(true);
+  });
+});
