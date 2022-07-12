2682ab367deed0b30126dab21e80ae6247e5cc0b
dduckddak-kim <kkgg7527@gmail.com>
2022-07-12 14:47:20 +0900

    Add. Modal 컴포넌트 추가
    - 홈화면 'edit' 버튼 클릭 시, 모달 컴포넌트 호출되도록 구현.

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="overlay" data-test="modal">
+    <div class="modal-container">
+
+      <div class="header">
+        <slot name="header">default header</slot>
+        <font-awesome-icon
+          icon="xmark"
+          class="close fa-2x"
+          @click="close"
+          data-test="modal-close" />
+      </div>
+
+      <div class="body">
+        <slot name="body">default body</slot>
+      </div>
+
+      <div class="footer">
+        <button class="button" @click="close">확인</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Modal',
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "src/style/color.scss";
+
+.overlay {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: $modalBgColor;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .modal-container {
+    width: 300px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: $white;
+    border-radius: 2px;
+
+    .header {
+      position: relative;
+
+      .close {
+        position: absolute;
+        top: -10px;
+        right: -15px;
+      }
+    }
+    .body {
+      margin: 20px 0;
+    }
+  }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
     <h1 data-test="title">Home</h1>
-    <button class="edit">
+    <button
+      class="edit"
+      @click="openModal"
+      data-test="mood-edit-button">
       <font-awesome-icon icon="edit" class="fa-xl" />
     </button>
   </div>
+
+  <AddMood
+    v-if="editModal.isVisible"
+    @close="closeModal"/>
 </template>
 
 <script>
+import AddMood from '@/components/Modal/Modal.vue';
+
 export default {
+  // eslint-disable-next-line vue/multi-word-component-names
   name: 'Footer',
-  props: {}
-}
+  components: {
+    AddMood,
+  },
+  props: {},
+  data() {
+    return {
+      editModal: {
+        isVisible: false,
+      },
+    };
+  },
+  methods: {
+    openModal() {
+      this.editModal.isVisible = true;
+    },
+    closeModal() {
+      this.editModal.isVisible = false;
+    },
+  },
+};
+
 </script>
 
 <style lang="scss">
