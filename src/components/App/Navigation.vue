691c00adf606d4df73a02e2b1a716ec1b4552603
dduckddak-kim <kkgg7527@gmail.com>
2022-07-12 14:49:44 +0900

    Fix. Navigation style 코드 수정
    - flex 관련 style 수정
    - eslint 규칙 적용

--- src/components/App/Navigation.vue
+++ src/components/App/Navigation.vue
@@ -1,20 +1,24 @@
 <template>
-  <nav class="flex-space-evenly flex-align-center" data-test="nav-wrapper">
+  <nav class="nav" data-test="nav-wrapper">
     <router-link
         v-for="menu of menus"
         :key="menu.id"
         :to="menu.url"
         class="link"
         :data-test="`nav-router-${ menu.name }`">
-      <font-awesome-icon :class="setActiveStyle(menu.url)" :icon="menu.icon" :data-test="`icon-${ menu.name }`" />
+      <font-awesome-icon
+          :class="setActiveStyle(menu.url)"
+          :icon="menu.icon"
+          :data-test="`icon-${ menu.name }`" />
     </router-link>
   </nav>
 </template>
 
 <script>
-import NavigationMenu from '@/assets/datas/navigationMenu'
+import NavigationMenu from '@/assets/datas/navigationMenu';
 
 export default {
+  // eslint-disable-next-line vue/multi-word-component-names
   name: 'Navigation',
   components: {},
   data() {
@@ -22,38 +26,41 @@ export default {
       menus: NavigationMenu,
       icon: {
       },
-    }
+    };
   },
   methods: {
     setActiveStyle(url) {
-      // let resultClass = this.$route.path === url ? "menu selected" : "menu";
+      let resultClass = 'menu fa-lg';
 
+      if (this.$route.path === url) resultClass += ' selected';
 
-      let resultClass = "menu fa-lg";
-      if (this.$route.path === url) resultClass += " selected"
       return resultClass;
-    }
-  }
-}
+    },
+  },
+};
 </script>
-
 
 <style lang="scss">
 @import "./src/style/color";
 
-nav {
-  width: 100%;
-  height: 8vh;
+.nav {
   position: fixed;
   bottom: 0;
+
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  width: 100%;
+  height: 8vh;
   background: $navigationBgColor;
-}
 
-.menu {
-  color: $dark-gray;
+  .menu {
+    color: $dark-gray;
 
-  &.selected {
-    color: $white;
+    &.selected {
+      color: $white;
+    }
   }
 }
 
