b9fdae3c2a35a76561192ac45a5f0d1f5c831ab5
Jieun <saxzvs@gmail.com>
2022-08-08 23:51:30 +0900

    Add. Login 구현 중

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,9 @@ import HomeRoutes from '@/router/HomeRoutes';
 import MoodRoutes from '@/router/MoodListRoutes';
 import YearsInPixels from '@/router/YearsInPixels';
 import MyPage from '@/router/MyPage';
+import Login from '@/router/Login';
 
-const routes = [].concat(HomeRoutes, MoodRoutes, YearsInPixels, MyPage);
+const routes = [].concat(HomeRoutes, MoodRoutes, YearsInPixels, MyPage, Login);
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
